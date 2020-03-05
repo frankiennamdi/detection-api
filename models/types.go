45728aba1acb69b0d13f3927022d1338bb0b7f3d
frankiennamdi <frankie4real@yahoo.com>
2020-03-04 23:20:00 -0500

    NFC: clean up

--- models/types.go
+++ models/types.go
@@ -30,34 +30,6 @@ type SuspiciousTravelResult struct {
 	SubsequentIPAccess             *RelatedAccessInfo `json:"subsequentIpAccess,omitempty"`
 }
 
-/*
-{
-  "currentGeo": {
-    "lat": 39.1702,
-    "lon": -76.8538,
-    "radius": 20
-  },
-  "travelToCurrentGeoSuspicious": true,
-  "travelFromCurrentGeoSuspicious": false,
-  "precedingIpAccess": {
-    "ip": "24.242.71.20",
-    "speed": 55,
-    "lat": 30.3764,
-    "lon": -97.7078,
-    "radius": 5,
-    "timestamp": 1514764800
-  },
-  "subsequentIpAccess": {
-    "ip": "91.207.175.104",
-    "speed": 27600,
-    "lat": 34.0494,
-    "lon": -118.2641,
-    "radius": 200,
-    "timestamp": 1514851200
-  }
-}
-*/
-
 type RelatedAccessInfo struct {
 	IP             string  `json:"ip"`
 	Speed          float64 `json:"speed"`
